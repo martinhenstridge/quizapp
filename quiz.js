5745282c9cec0f14e8e5692d712f8493dbf6a3b5
Martin Henstridge <mchenstridge@gmail.com>
2020-11-05 21:39:11 +0000

    Tidying, and remove dependency on immutable library

--- quiz.js
+++ quiz.js
@@ -1,3 +1,5 @@
+"use strict";
+
 
 // States
 const STATE_OPEN   = 1;
@@ -5,7 +7,7 @@ const STATE_FLUX   = 2;
 const STATE_LOCKED = 3;
 
 // Events
-const EVENT_ASK           = 1
+const EVENT_ASK           = 1;
 const EVENT_EDIT_START    = 2;
 const EVENT_EDIT_COMPLETE = 3;
 const EVENT_REMOTE_UPDATE = 4;
@@ -13,32 +15,57 @@ const EVENT_LOCK          = 5;
 const EVENT_REVEAL        = 6;
 
 
-let Question = Immutable.Record({
-    state: null,
-    text: "",
-    guess: "",
-    answer: "",
-});
+function Quiz(selector) {
+    this.node = document.querySelector(selector);
+    this.questions = new Map();
+    Object.freeze(this);
+};
 
-let QuestionDom = Immutable.Record({
-    locked: true,
-    text: "",
-    guess: "",
-    answer: "",
-});
+Quiz.prototype.update = function (evt) {
+    console.debug(`event: ${JSON.stringify(evt)}`);
 
+    const question = this.questions.get(evt.number);
+    const updated = update_question(question, evt);
+    this.questions.set(evt.number, updated);
 
-function calculate_dom_state(question) {
-    if (typeof question === "undefined") {
-        return undefined;
+    let node;
+    if (typeof question !== "undefined") {
+        node = get_question_node(evt.number);
+    } else {
+        node = new_question_node(evt.number);
+        this.node.appendChild(node);
     }
-    return QuestionDom({
-        locked: question.state === STATE_LOCKED,
-        text: question.text,
-        guess: question.guess,
-        answer: question.answer,
-    });
+    set_question_node(node, updated);
+};
+
+
+function Question(state, text, guess, answer, domnode) {
+    this.state = state;
+    this.text = text;
+    this.guess = guess;
+    this.answer = answer;
+    Object.freeze(this);
 }
+
+Question.prototype.toString = function () {
+    return `Question(${this.state}, ${this.text}, ${this.guess}, ${this.answer})`;
+};
+
+Question.prototype.set_state = function (state) {
+    return new Question(state, this.text, this.guess, this.answer)
+};
+
+Question.prototype.set_text = function (text) {
+    return new Question(this.state, text, this.guess, this.answer)
+};
+
+Question.prototype.set_guess = function (guess) {
+    return new Question(this.state, this.text, guess, this.answer)
+};
+
+Question.prototype.set_answer = function (answer) {
+    return new Question(this.state, this.text, this.guess, answer)
+};
 
 
 function h(tag, props, children) {
@@ -52,112 +79,70 @@ function h(tag, props, children) {
     return elem;
 }
 
-function append_dom(root, number, dom) {
-    root.appendChild(
-        h("div", {
-            id: `Q${number}`,
-            className: "question",
-        }, [
-            h("div", {}, [
-                h("span", {
-                    innerText: `[Q${number}] `,
-                }, []),
-                h("span", {
-                    id: `Q${number}__text`,
-                    innerText: dom.text,
-                }, []),
-            ]),
-            h("div", {}, [
-                h("input", {
-                    id: `Q${number}__guess`,
-                    value: dom.guess,
-                    disabled: dom.locked,
-                }, []),
-            ]),
-            h("div", {}, [
-                h("p", {
-                    id: `Q${number}__answer`,
-                    innerText: dom.answer,
-                }, []),
-            ]),
-        ]),
-    );
-}
 
-function append_dom_old(root, number, dom) {
-    let container = document.createElement("div");
-    container.className = "question";
-    container.id = `Q${number}`;
+function new_question_node(number) {
+    const node_number = h("span", {
+        className: "question_number",
+        innerText: `[Q${number}] `
+    }, []);
+    const node_text = h("span", {
+        className: "question_text"
+    }, []);
+    const node_guess = h("input", {
+        className: "question_guess"
+    }, []);
+    const node_answer = h("p", {
+        className: "question_answer"
+    }, []);
 
-    let qcontainer = document.createElement("div");
-    let gcontainer = document.createElement("div");
-    let acontainer = document.createElement("div");
+    node_guess.addEventListener("focusin", function (e) {
+        quiz.update({
+            number: number,
+            type: EVENT_EDIT_START,
+        });
+        e.stopPropagation()
+    });
+    node_guess.addEventListener("focusout", function (e) {
+        quiz.update({
+            number: number,
+            type: EVENT_EDIT_COMPLETE,
+            guess: e.target.value,
+        });
+        e.stopPropagation()
+    });
 
-    let node_number = document.createElement("span");
-    node_number.innerText = `[Q${number}] `;
-
-    let node_text = document.createElement("span");
-    node_text.id = `Q${number}__text`;
-    node_text.innerText = dom.text;
-
-    let node_guess = document.createElement("input");
-    node_guess.id = `Q${number}__guess`;
-    node_guess.value = dom.guess;
-    node_guess.disabled = dom.locked;
-
-    let node_answer = document.createElement("p");
-    node_answer.id = `Q${number}__answer`;
-    node_answer.innerText = dom.answer;
-
-    qcontainer.appendChild(node_number);
-    qcontainer.appendChild(node_text);
-    gcontainer.appendChild(node_guess);
-    acontainer.appendChild(node_answer);
-
-    container.appendChild(qcontainer);
-    container.appendChild(gcontainer);
-    container.appendChild(acontainer);
-
-    root.appendChild(container);
+    return h("div", { id: `Q${number}`, className: "question" }, [
+        h("div", {}, [node_number, node_text]),
+        h("div", {}, [node_guess]),
+        h("div", {}, [node_answer]),
+    ]);
 }
 
 
-function edit_dom(elem, number, wanted) {
-    let text = elem.querySelector(`#Q${number}__text`);
-    let guess = elem.querySelector(`#Q${number}__guess`);
-    let answer = elem.querySelector(`#Q${number}__answer`);
-
-    text.innerText = wanted.text;
-    guess.value = wanted.guess;
-    guess.disabled = wanted.locked;
-    answer.innerText = wanted.answer;
+function get_question_node(number) {
+    return document.querySelector(`div#Q${number}`);
 }
 
 
-function update_dom(number, wanted) {
-    const root = document.querySelector("div#quiz");
-    const elem = root.querySelector(`div#Q${number}`);
+function set_question_node(node, updated) {
+    let text = node.querySelector(".question_text");
+    let guess = node.querySelector(".question_guess");
+    let answer = node.querySelector(".question_answer");
 
-    if (elem === null) {
-        append_dom(root, number, wanted);
+    text.innerText = updated.text;
+
+    if (updated.state !== STATE_FLUX) {
+        guess.value = updated.guess;
+        guess.disabled = (updated.state === STATE_LOCKED);
+    }
+
+    if (updated.answer === null) {
+        answer.hidden = true;
+        answer.innerText = "";
     } else {
-        edit_dom(elem, number, wanted);
+        answer.hidden = false;
+        answer.innerText = updated.answer;
     }
-}
-
-
-function update_quiz(quiz, data) {
-    const q_old = quiz.get(data.number);
-    const q_new = update_question(q_old, data);
-
-    const dom_old = calculate_dom_state(q_old);
-    const dom_new = calculate_dom_state(q_new);
-
-    if (!Immutable.is(dom_new, dom_old)) {
-        update_dom(data.number, dom_new, dom_old);
-    }
-
-    return quiz.set(data.number, q_new);
 }
 
 
@@ -192,7 +177,7 @@ function post_event_ask(question, data) {
         throw "Duplicate question";
     }
 
-    return Question({ state: STATE_OPEN, text: data.text });
+    return new Question(STATE_OPEN, data.text, null, null);
 }
 
 
@@ -203,7 +188,7 @@ function post_event_edit_start(question, data) {
 
     switch (question.state) {
         case STATE_OPEN:
-            return question.set("state", STATE_FLUX);
+            return question.set_state(STATE_FLUX);
 
         case STATE_FLUX:
             throw "Edit already in progress";
@@ -224,7 +209,7 @@ function post_event_edit_complete(question, data) {
 
     switch (question.state) {
         case STATE_FLUX:
-            return question.set("state", STATE_OPEN).set("guess", data.guess);
+            return question.set_state(STATE_OPEN).set_guess(data.guess);
 
         case STATE_OPEN:
             throw "No edit in progress";
@@ -246,10 +231,10 @@ function post_event_remote_update(question, data) {
 
     switch (question.state) {
         case STATE_OPEN:
-            return question.set("guess", data.guess);
+            return question.set_guess(data.guess);
 
         case STATE_FLUX:
-            console.log("Local edit in progress, ignoring");
+            console.debug("Local edit in progress, ignoring");
             return question;
 
         case STATE_LOCKED:
@@ -269,7 +254,7 @@ function post_event_lock(question, data) {
     switch (question.state) {
         case STATE_OPEN:
         case STATE_FLUX:
-            return question.set("state", STATE_LOCKED);
+            return question.set_state(STATE_LOCKED);
 
         case STATE_LOCKED:
             throw "Duplicate lock";
@@ -287,7 +272,7 @@ function post_event_reveal(question, data) {
 
     switch (question.state) {
         case STATE_LOCKED:
-            return question.set("answer", data.answer);
+            return question.set_answer(data.answer);
 
         case STATE_OPEN:
         case STATE_FLUX:
@@ -299,57 +284,30 @@ function post_event_reveal(question, data) {
 }
 
 
-function handle_focusin(quiz, e) {
-    const target = e.target;
-    if (target.tagName !== "INPUT") {
-        return;
-    }
-
-    const number = target.parentNode.parentNode.id;
-    // post_event(EVENT_EDIT_START, {number: number});
-
-    e.stopPropagation()
-}
-
-
-function handle_focusout(quiz, e) {
-    const target = e.target;
-    if (target.tagName !== "INPUT") {
-        return;
-    }
-
-    const number = target.parentNode.parentNode.id;
-    // post_event(EVENT_EDIT_COMPLETE, {number: number, guess: target.value});
-
-    e.stopPropagation()
-}
-
-
-function main() {
-    const area = document.querySelector("div.quiz");
-    area.addEventListener("focusin", handle_focusin);
-    area.addEventListener("focusout", handle_focusout);
-}
-
-
-let quiz = Immutable.OrderedMap();
 let events = [
     { number: 1, type: EVENT_ASK, text: "What is 1+1?" },
-    { number: 1, type: EVENT_REMOTE_UPDATE, guess: "2" },
-    { number: 1, type: EVENT_EDIT_START },
-    { number: 1, type: EVENT_EDIT_COMPLETE, guess: "2" },
     { number: 2, type: EVENT_ASK, text: "What is 2+2?" },
-    { number: 2, type: EVENT_EDIT_START },
-    { number: 2, type: EVENT_REMOTE_UPDATE, guess: "1" },
-    { number: 2, type: EVENT_EDIT_COMPLETE, guess: "4" },
+    { number: 1, type: EVENT_REMOTE_UPDATE, guess: "1" },
+    { number: 3, type: EVENT_ASK, text: "What is 3+3?" },
+    { number: 4, type: EVENT_ASK, text: "What is 4+4?" },
+    { number: 5, type: EVENT_ASK, text: "What is 5+5?" },
+    { number: 1, type: EVENT_LOCK },
+    { number: 2, type: EVENT_LOCK },
+    { number: 3, type: EVENT_LOCK },
+    { number: 4, type: EVENT_LOCK },
+    { number: 5, type: EVENT_LOCK },
+    { number: 1, type: EVENT_REVEAL, answer: "2" },
+    { number: 2, type: EVENT_REVEAL, answer: "4" },
+    { number: 3, type: EVENT_REVEAL, answer: "6" },
+    { number: 4, type: EVENT_REVEAL, answer: "8" },
+    { number: 5, type: EVENT_REVEAL, answer: "10" },
 ]
 
+let quiz = new Quiz("div#quiz");
 for (let i = 0; i < events.length; i++) {
     setTimeout(() => {
         try {
-            console.debug(`event: ${JSON.stringify(events[i])}`);
-            quiz = update_quiz(quiz, events[i]);
-            console.debug(`state: ${JSON.stringify(quiz)}`);
+            quiz.update(events[i]);
         } catch (err) {
             console.error(err);
         }
