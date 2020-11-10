e99bebf804a9e13b9eb80781cbc3da7a4c6bc7b9
Martin Henstridge <mchenstridge@gmail.com>
2020-11-10 18:18:14 +0000

    Play landing page

--- quiz/views/play.py
+++ quiz/views/play.py
@@ -1,5 +1,15 @@
+from collections import defaultdict
+from flask import render_template, url_for
+from ..quiz import Quiz
+
+
 def page(key):
-    return f"[{key}] LIST PLAYERS HERE..."
+    quiz = Quiz.get(key)
+    teams = defaultdict(list)
+    for name, team in quiz.players:
+        url = url_for("handler_play_player", key=key, name=name)
+        teams[team].append((name, url))
+    return render_template("play.html", key=key, teams=teams)
 
 
 def page_player(key, player):
