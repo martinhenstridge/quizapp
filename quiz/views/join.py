4e5454be56eae4dacb4d2cd07b2c0f5400ceac29
Martin Henstridge <mchenstridge@gmail.com>
2020-11-27 21:25:49 +0000

    Tinkering

--- quiz/views/join.py
+++ quiz/views/join.py
@@ -11,16 +11,16 @@ def join(quizid):
         return render_template("join.html", quizid=quizid, teams=quiz.teams)
 
     team = request.form["team"]
-    dest = url_for("auth", quizid=quizid, team=team)
+    dest = url_for("login", quizid=quizid, team=team)
     return redirect(dest)
 
 
-@app.route("/<quizid>/join/<team>", methods=["GET", "POST"])
-def auth(quizid, team):
+@app.route("/<quizid>/login/<team>", methods=["GET", "POST"])
+def login(quizid, team):
     quiz = Quiz.get(quizid)
 
     if request.method == "GET":
-        return render_template("auth.html", quizid=quizid, team=team)
+        return render_template("login.html", quizid=quizid, team=team)
 
     player = request.form["name"]
     password = request.form["token"]
@@ -28,7 +28,7 @@ def auth(quizid, team):
     # On incorrect password redirect to same page with flashed message.
     if not quiz.check_team_password(team, password.upper()):
         flash(f"Incorrect password for team {team}")
-        dest = url_for("auth", quizid=quizid, team=team)
+        dest = url_for("login", quizid=quizid, team=team)
         return redirect(dest)
 
     session.clear()
